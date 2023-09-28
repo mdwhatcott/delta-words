a56a90f14a132c275c24fc2e75d9f012443f1f09
Michael Whatcott <mike@smarty.com>
2023-09-28 10:00:15 -0700

    Trying to gather sets of letters.
    So far:
    
    [ruendb rledig teacnr telfri rgsine tporse seratm beworl nregda sirpet]

--- cmd/delta/main.go
+++ cmd/delta/main.go
@@ -6,54 +6,65 @@ import (
 	"math/rand"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"github.com/mdwhatcott/delta-words/lib"
 	"github.com/mdwhatcott/funcy"
 	"github.com/mdwhatcott/go-set/v2/set"
 	"github.com/mdwhatcott/must/osmust"
+	"github.com/mdwhatcott/tui/v2"
 )
 
 var Version = "dev"
 
-type Config struct {
-	letters string
-	dict    string
-}
-
 func main() {
-	var config Config
 	flags := flag.NewFlagSet(fmt.Sprintf("%s @ %s", filepath.Base(os.Args[0]), Version), flag.ExitOnError)
-	flags.StringVar(&config.letters, "letters", "singer", "The letters to make words out of.")
-	flags.StringVar(&config.dict, "dict", "/usr/share/dict/words", "The path of the dictionary file.")
 	_ = flags.Parse(os.Args[1:])
 
-	dict := set.Of(strings.Split(strings.ToLower(string(osmust.ReadFile(config.dict))), "\n")...).Slice()
-	if config.letters == "" {
-		var candidates []string
-		for _, word := range dict {
-			if len(word) == 6 {
-				candidates = append(candidates, word)
-			}
-		}
-		config.letters = candidates[rand.Intn(len(candidates))]
-	}
+	ui := tui.New()
 
-	config.letters = string(funcy.Shuffle([]byte(config.letters)))
-	fmt.Println("LETTERS:", config.letters)
-
-	matches := lib.FindMatches(config.letters, dict...)
-	lengths := make(map[int][]string)
-	for _, match := range matches {
-		lengths[len(match)] = append(lengths[len(match)], match)
-	}
-	for x := 3; x <= len(config.letters); x++ {
-		if len(lengths[x]) == 0 {
+	path := "/usr/share/dict/words"
+	dict := set.Of(strings.Split(strings.ToLower(string(osmust.ReadFile(path))), "\n")...).Slice()
+	var candidates []string
+	for _, word := range dict {
+		if strings.Contains(word, " ") {
 			continue
 		}
-		fmt.Println(x)
-		for _, match := range lengths[x] {
-			fmt.Println(match)
+		if len(word) == 6 {
+			candidates = append(candidates, word)
 		}
 	}
+	fmt.Println("candidates:", len(candidates))
+
+	const minOptions = 5
+	goodLetters := set.Make[string](0)
+	for {
+		letters := sortString(candidates[rand.Intn(len(candidates))])
+		lengths := funcy.SlicedIndexBy(funcy.ByLength[string], lib.FindMatches(letters, dict...))
+		if len(lengths[3]) < minOptions || len(lengths[4]) < minOptions || len(lengths[5]) < minOptions || len(lengths[6]) < minOptions {
+			continue
+		}
+		fmt.Println()
+		fmt.Println("LETTERS:", letters)
+		for _, match := range lengths[6] {
+			fmt.Println(match)
+		}
+		if ui.NoYes(fmt.Sprintf("%d/%d - Keep?", goodLetters.Len(), 10)) {
+			goodLetters.Add(letters)
+		}
+		if goodLetters.Len() == 10 {
+			result := goodLetters.Slice()
+			sort.Strings(result)
+			fmt.Println(result)
+			break
+
+		}
+	}
+}
+
+func sortString(s string) string {
+	b := []byte(s)
+	sort.Slice(b, func(i, j int) bool { return b[i] < b[j] })
+	return string(b)
 }
